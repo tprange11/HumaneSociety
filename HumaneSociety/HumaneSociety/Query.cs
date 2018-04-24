6de2aea6aa781be0f7f56884b13ad6d3fa5711f0
Joel Corey <joelcorey@fastmail.com>
2018-04-24 15:48:09 -0600

    Added back stubbed methods

--- HumaneSociety/HumaneSociety/Query.cs
+++ HumaneSociety/HumaneSociety/Query.cs
@@ -8,8 +8,83 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        
-        
-      
+
+        public static int UpdateAdoption()
+        {
+            return 1;
+        }
+
+        public static int GetPendingAdoptions()
+        {
+            return 1;
+        }
+
+        public static int GetShots()
+        {
+            return 1;
+        }
+
+        public static int UpdateShot()
+        {
+            return 1;
+        }
+
+        public static int EnterUpdate()
+        {
+            return 1;
+        }
+
+        public static int GetBreed()
+        {
+            return 1;
+        }
+
+        public static int GetDiet()
+        {
+            return 1;
+        }
+
+        public static int RemoveAnimal()
+        {
+            return 1;
+        }
+
+        public static int GetLocation()
+        {
+            return 1;
+        }
+
+        public static int AddAnimal()
+        {
+            return 1;
+        }
+
+        public static int EmployeeLogin()
+        {
+            return 1;
+        }
+
+        public static int EmployeeLogin(string userName, string password)
+        {
+            return 1;
+        }
+
+        public static int RetrieveEmployeeUser(string email, int employeeNumber)
+        {
+            return 1;
+        }
+
+        public static int AddUsernameAndPassword(Employee employee, string password)
+        {
+            return 1;
+        }
+
+        public static int CheckEmployeeUserNameExist(username)
+        {
+            return 1;
+        }
     }
+
+
+}
 }
