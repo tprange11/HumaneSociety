90c128858e5ddd5fc880c22d40f177a7ad02c4b1
tprange11 <tprange@gmail.com>
2018-04-25 07:34:20 -0500

    Adding missing functions

--- HumaneSociety/HumaneSociety/Query.cs
+++ HumaneSociety/HumaneSociety/Query.cs
@@ -8,8 +8,21 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        
-        
-      
+        internal static int? GetBread()
+        {
+            HumaneSocietyDataContext db = new HumaneSocietyDataContext();
+            
+                return 0;
+        }
+
+        internal static void RunEmployeeQueries(Employee employee, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Client GetClient(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
