98b3a54673d5ae17ea5b034631c0e4322d925bdb
Joel Corey <joelcorey@fastmail.com>
2018-04-25 06:31:42 -0600

    Query method added, comment added

--- HumaneSociety/HumaneSociety/Query.cs
+++ HumaneSociety/HumaneSociety/Query.cs
@@ -8,57 +8,44 @@ namespace HumaneSociety
 {
     public static class Query
     {
+        // Attempting to order methods/delegates in order of CRUD 
+        // The more simple the method the more likely it can be a delegate
 
-        public static int UpdateAdoption()
+        public delegate string AddAnimal(string derp);
+        public delegate string UpdateAdoption(string derp);
+        public delegate string UpdateShot(string derp);
+        public delegate string EnterUpdate(string derp);
+        public delegate string RemoveAnimal(string derp);
+
+        public delegate string GetPendingAdoptions(string derp);
+        public delegate string GetShots(string derp);
+        public delegate string GetBreed(string derp);
+        public delegate string GetDiet(string derp);
+        public delegate string GetLocation(string derp);
+
+        public static void CreateSql()
         {
-            return 1;
+
+        }
+        public static void ReplaceSql()
+        {
+
+        }
+        public static void UpdateSql()
+        {
+
+        }
+        public static void DeleteSql()
+        {
+
         }
 
-        public static int GetPendingAdoptions()
+        // Simple queries done with this method
+        public static void QuerySql()
         {
-            return 1;
-        }
 
-        public static int GetShots()
-        {
-            return 1;
         }
-
-        public static int UpdateShot()
-        {
-            return 1;
-        }
-
-        public static int EnterUpdate()
-        {
-            return 1;
-        }
-
-        public static int GetBreed()
-        {
-            return 1;
-        }
-
-        public static int GetDiet()
-        {
-            return 1;
-        }
-
-        public static int RemoveAnimal()
-        {
-            return 1;
-        }
-
-        public static int GetLocation()
-        {
-            return 1;
-        }
-
-        public static int AddAnimal()
-        {
-            return 1;
-        }
-
+        // Employee stuff kept seperate from animal stuff
         public static int EmployeeLogin()
         {
             return 1;
@@ -79,12 +66,9 @@ namespace HumaneSociety
             return 1;
         }
 
-        public static int CheckEmployeeUserNameExist(username)
+        public static int CheckEmployeeUserNameExist(string username)
         {
             return 1;
         }
     }
-
-
-}
 }
