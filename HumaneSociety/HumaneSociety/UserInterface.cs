2ecb502300a015141484f45556667ba6f738f3dd
tprange11 <tprange@gmail.com>
2018-04-26 08:42:09 -0500

    ReOrdered function

--- HumaneSociety/HumaneSociety/UserInterface.cs
+++ HumaneSociety/HumaneSociety/UserInterface.cs
@@ -1,48 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HumaneSociety
 {
     public static class UserInterface
     {
+        private static string BoolToYesNo(bool? input)
+        {
+            if (input == true)
+            {
+                return "yes";
+            }
+            else
+            {
+                return "no";
+            }
+        }
+
+        internal static void DisplayAnimals(List<Animal> animals)
+        {
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.ID + " " + animal.name + " " + animal.Breed1.breed1);
+            }
+        }
+
+        public static void DisplayAnimalInfo(Animal animal)
+        {
+            List<string> info = new List<string>() { "ID: " + animal.ID, animal.name, animal.age + "years old", "Demeanour: " + animal.demeanor, "Kid friendly: " + BoolToYesNo(animal.kidFriendly), "pet friendly: " + BoolToYesNo(animal.petFriendly), $"Location: {animal.Room.name} in {animal.Room.building}", "Weight: " + animal.weight.ToString(), "Food amoumnt:" + animal.DietPlan.amount };
+            DisplayUserOptions(info);
+            Console.ReadLine();
+        }
+
+        internal static void DisplayClientInfo(Client client)
+        {
+            List<string> info = new List<string>() { client.firstName, client.lastName, client.email, "Number of kids: " + client.kids.ToString(), "Home size: " + client.homeSize.ToString(), "Income: " + client.income.ToString(), client.UserAddress1.USState.name };
+            DisplayUserOptions(info);
+            Console.ReadLine();
+        }
+
         public static void DisplayUserOptions(List<string> options)
         {
-            foreach(string option in options)
+            foreach (string option in options)
             {
                 Console.WriteLine(option);
             }
         }
+
         public static void DisplayUserOptions(string options)
         {
             Console.WriteLine(options);
         }
-        public static string GetUserInput()
-        {
-            string input = Console.ReadLine();
-            switch (input.ToLower())
-            {
-                case "reset":
-                    PointOfEntry.Run();
-                    Environment.Exit(1);
-                    break;
-                case "exit":
-                    Environment.Exit(1);
-                    break;
-                default:
-                    break;
-            }
 
-            return input;
-        }
-        public static string GetStringData(string parameter, string target)
+        public static Dictionary<int, string> EnterSearchCriteria(Dictionary<int, string> searchParameters, string input)
         {
-            string data;
-            DisplayUserOptions($"What is {target} {parameter}?");
-            data = GetUserInput();
-            return data;
+            Console.Clear();
+            switch (input)
+            {
+                case "1":
+                    searchParameters.Add(1, UserInterface.GetStringData("categories", "the animal's"));
+                    return searchParameters;
+
+                case "2":
+                    searchParameters.Add(2, UserInterface.GetStringData("breed", "the animal's"));
+                    return searchParameters;
+
+                case "3":
+                    searchParameters.Add(3, UserInterface.GetStringData("name", "the animal's"));
+                    return searchParameters;
+
+                case "4":
+                    searchParameters.Add(4, UserInterface.GetIntegerData("age", "the animal's").ToString());
+                    return searchParameters;
+
+                case "5":
+                    searchParameters.Add(5, UserInterface.GetStringData("demeanor", "the animal's"));
+                    return searchParameters;
+
+                case "6":
+                    searchParameters.Add(6, UserInterface.GetBitData("the animal", "kid friendly").ToString());
+                    return searchParameters;
+
+                case "7":
+                    searchParameters.Add(7, UserInterface.GetBitData("the animal", "pet friendly").ToString());
+                    return searchParameters;
+
+                case "8":
+                    searchParameters.Add(8, UserInterface.GetIntegerData("weight", "the animal's").ToString());
+                    return searchParameters;
+
+                case "9":
+                    searchParameters.Add(9, UserInterface.GetIntegerData("ID", "the animal's").ToString());
+                    return searchParameters;
+
+                default:
+                    UserInterface.DisplayUserOptions("Input not recognized please try agian");
+                    return searchParameters;
+            }
+        }
+
+        public static bool? GetBitData()
+        {
+            string input = GetUserInput();
+            if (input.ToLower() == "yes" || input.ToLower() == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         internal static bool? GetBitData(List<string> options)
@@ -58,18 +125,7 @@ namespace HumaneSociety
                 return false;
             }
         }
-        public static bool? GetBitData()
-        {
-            string input = GetUserInput();
-            if (input.ToLower() == "yes" || input.ToLower() == "y")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+
         internal static bool? GetBitData(string target, string parameter)
         {
             DisplayUserOptions($"Is {target} {parameter}?");
@@ -84,26 +140,17 @@ namespace HumaneSociety
             }
         }
 
-        internal static void DisplayAnimals(List<Animal> animals)
+        public static bool GetBitData(string option)
         {
-            foreach(Animal animal in animals)
+            DisplayUserOptions(option);
+            string input = GetUserInput();
+            if (input.ToLower() == "yes" || input.ToLower() == "y")
             {
-                Console.WriteLine(animal.ID + " " + animal.name + " " + animal.Breed1.breed1);
+                return true;
             }
-        }
-
-        internal static int GetIntegerData()
-        {
-            try
+            else
             {
-                int data = int.Parse(GetUserInput());
-                return data;
-            }
-            catch
-            {
-                Console.Clear();
-                DisplayUserOptions("Incorrect input please enter an integer number.");
-                return GetIntegerData();
+                return false;
             }
         }
 
@@ -122,82 +169,48 @@ namespace HumaneSociety
             }
         }
 
-        internal static void DisplayClientInfo(Client client)
+        public static string GetStringData(string parameter, string target)
         {
-            List<string> info = new List<string>() { client.firstName, client.lastName, client.email, "Number of kids: " + client.kids.ToString(), "Home size: " + client.homeSize.ToString(), "Income: " + client.income.ToString(), client.UserAddress1.USState.name };
-            DisplayUserOptions(info);
-            Console.ReadLine();
+            string data;
+            DisplayUserOptions($"What is {target} {parameter}?");
+            data = GetUserInput();
+            return data;
         }
 
-        public static void DisplayAnimalInfo(Animal animal)
+        internal static int GetIntegerData()
         {
-            List<string> info = new List<string>() {"ID: " + animal.ID, animal.name, animal.age + "years old", "Demeanour: " + animal.demeanor, "Kid friendly: " + BoolToYesNo(animal.kidFriendly), "pet friendly: " + BoolToYesNo(animal.petFriendly), $"Location: {animal.Room.name} in {animal.Room.building}", "Weight: " + animal.weight.ToString(),  "Food amoumnt:" + animal.DietPlan.amount};
-            DisplayUserOptions(info);
-            Console.ReadLine();
-
-        }
-
-        private static string BoolToYesNo(bool? input)
-        {
-            if (input == true)
+            try
             {
-                return "yes";
+                int data = int.Parse(GetUserInput());
+                return data;
             }
-            else
+            catch
             {
-                return "no";
+                Console.Clear();
+                DisplayUserOptions("Incorrect input please enter an integer number.");
+                return GetIntegerData();
             }
         }
 
-        public static bool GetBitData(string option)
+        public static string GetUserInput()
         {
-            DisplayUserOptions(option);
-            string input = GetUserInput();
-            if (input.ToLower() == "yes" || input.ToLower() == "y")
+            string input = Console.ReadLine();
+            switch (input.ToLower())
             {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static Dictionary<int, string> EnterSearchCriteria(Dictionary<int, string> searchParameters, string input)
-        {
-            Console.Clear();
-            switch (input)
-            {
-                case "1":
-                    searchParameters.Add(1, UserInterface.GetStringData("categories", "the animal's"));
-                    return searchParameters;
-                case "2":
-                    searchParameters.Add(2, UserInterface.GetStringData("breed", "the animal's"));
-                    return searchParameters;
-                case "3":
-                    searchParameters.Add(3, UserInterface.GetStringData("name", "the animal's"));
-                    return searchParameters;
-                case "4":
-                    searchParameters.Add(4, UserInterface.GetIntegerData("age", "the animal's").ToString());
-                    return searchParameters;
-                case "5":
-                    searchParameters.Add(5, UserInterface.GetStringData("demeanor", "the animal's"));
-                    return searchParameters;
-                case "6":
-                    searchParameters.Add(6, UserInterface.GetBitData("the animal", "kid friendly").ToString());
-                    return searchParameters;
-                case "7":
-                    searchParameters.Add(7, UserInterface.GetBitData("the animal", "pet friendly").ToString());
-                    return searchParameters;
-                case "8":
-                    searchParameters.Add(8, UserInterface.GetIntegerData("weight", "the animal's").ToString());
-                    return searchParameters;
-                case "9":
-                    searchParameters.Add(9, UserInterface.GetIntegerData("ID", "the animal's").ToString());
-                    return searchParameters;
+                case "reset":
+                    PointOfEntry.Run();
+                    Environment.Exit(1);
+                    break;
+
+                case "exit":
+                    Environment.Exit(1);
+                    break;
+
                 default:
-                    UserInterface.DisplayUserOptions("Input not recognized please try agian");
-                    return searchParameters;
+                    break;
             }
+
+            return input;
         }
     }
 }
